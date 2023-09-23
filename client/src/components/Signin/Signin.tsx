aeee709d50f967fda636426dc9e0372e96b191c4
Sikuani <alpha197511@hotmail.com>
2023-09-23 00:22:55 -0400

    Fix typo username

--- client/src/components/Signin/Signin.tsx
+++ client/src/components/Signin/Signin.tsx
@@ -67,7 +67,7 @@ const Signin = () => {
           onChange={(e) => setUsername(e.target.value)}
         />
 
-        <label htmlFor="password">Username</label>
+        <label htmlFor="password">Password</label>
         <input
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           type="password"
